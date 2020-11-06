65f2e7e29c8792ae42d1aec37dc32e03539ab81d
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2020-11-06 13:30:47 +0000

    Apply fixes from StyleCI

--- src/Facades/Annotation.php
+++ src/Facades/Annotation.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static string|null summary(string $controller, string $method = null)
  * @method static string|null description(string $controller, string $method = null)
- *
  * @method static boolean isDeprecated(string $controller, string $method = null)
  * @method static boolean isDeprecatedClass(string $controller)
  * @method static boolean isDeprecatedMethod(string $controller, string $method)
