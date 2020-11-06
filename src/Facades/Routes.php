1e80718d1c061e80da72dd7e751839cb36404353
Andrey Helldar <helldar@ai-rus.com>
2020-11-06 14:22:30 +0300

    Fixed doc-block

--- src/Facades/Routes.php
+++ src/Facades/Routes.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Facades\Facade;
  * @method static Collection collection()
  * @method static Support hideMethods(array $methods)
  * @method static Support hideMatching(array $matching)
+ * @method static Support setHideMethods(array $methods)
+ * @method static Support setHideMatching(array $matching)
+ * @method static Support setDomainForce(bool $force = false)
+ * @method static Support setUrl(string $url)
+ * @method static Support setNamespace(string $namespace = null)
  */
 final class Routes extends Facade
 {
