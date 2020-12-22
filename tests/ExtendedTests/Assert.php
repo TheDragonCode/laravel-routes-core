0aa6704ed91e84fa5bf09fa1b4100920ea62d693
Andrey Helldar <helldar@ai-rus.com>
2020-12-22 12:57:46 +0300

    Updated tests

--- tests/ExtendedTests/Assert.php
+++ tests/ExtendedTests/Assert.php
@@ -2,26 +2,42 @@
 
 namespace Tests\ExtendedTests;
 
-use PHPUnit\Framework\Constraint\IsType;
+use PHPUnit\Framework\ExpectationFailedException;
 
 /** @mixin \Tests\TestCase */
 trait Assert
 {
+    /**
+     * Asserts that a variable is of type array.
+     *
+     * @throws ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     *
+     * @psalm-assert array $actual
+     */
     public static function assertIsArray($actual, string $message = ''): void
     {
-        static::assertThat(
-            $actual,
-            new IsType(IsType::TYPE_ARRAY),
-            $message
-        );
+        if (method_exists(parent::class, 'assertIsArray')) {
+            parent::assertIsArray($actual, $message);
+        } else {
+            static::assertTrue(is_array($actual), $message);
+        }
     }
 
-    public static function assertIsInt($actual, string $message = null): void
+    /**
+     * Asserts that a variable is of type int.
+     *
+     * @throws ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     *
+     * @psalm-assert int $actual
+     */
+    public static function assertIsInt($actual, string $message = ''): void
     {
-        static::assertThat(
-            $actual,
-            new IsType(IsType::TYPE_INT),
-            $message
-        );
+        if (method_exists(parent::class, 'assertIsArray')) {
+            parent::assertIsInt($actual, $message);
+        } else {
+            static::assertTrue(is_int($actual), $message);
+        }
     }
 }
