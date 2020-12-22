38cddc7f9b29c5cca12188326600e41d87c52e4f
Andrey Helldar <helldar@ai-rus.com>
2020-12-22 12:49:03 +0300

    Updated tests

--- tests/ExtendedTests/Assert.php
+++ tests/ExtendedTests/Assert.php
@@ -9,27 +9,19 @@ trait Assert
 {
     public static function assertIsArray($actual, string $message = ''): void
     {
-        if (method_exists(static::class, 'assertIsArray')) {
-            static::assertIsArray($actual, $message);
-        } else {
-            static::assertThat(
-                $actual,
-                new IsType(IsType::TYPE_ARRAY),
-                $message
-            );
-        }
+        static::assertThat(
+            $actual,
+            new IsType(IsType::TYPE_ARRAY),
+            $message
+        );
     }
 
     public static function assertIsInt($actual, string $message = null): void
     {
-        if (method_exists(static::class, 'assertIsInt')) {
-            static::assertIsInt($actual, $message);
-        } else {
-            static::assertThat(
-                $actual,
-                new IsType(IsType::TYPE_INT),
-                $message
-            );
-        }
+        static::assertThat(
+            $actual,
+            new IsType(IsType::TYPE_INT),
+            $message
+        );
     }
 }
